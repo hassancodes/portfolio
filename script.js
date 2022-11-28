215690fbb0c20ea3a7fd952d516da6272a04aec8
Muhammad hassan <hassanhanjra900@gmail.com>
2022-11-28 00:46:43 -0500

    fix:burger animation

--- script.js
+++ script.js
@@ -9,17 +9,38 @@ burger.addEventListener('click' , ()=>{
         gsap.set("body", {overflowX:"hidden"});
         gsap.set(".burger", {color:"black"});
 
-    }else{
-        if(burger.classList.contains("active")){
+    }
+    else
+    {
+     if(burger.classList.contains("active")){
         gsap.to(".nav-list", {x:"0%"});
         gsap.to(".nav-list", {stroke:"black", y:"0"})
         gsap.fromTo(".nav-list .nav-elements", {opacity:0} , {opacity:1,y:"25",delay:0.15, stagger:0.35})
         gsap.set("body", {overflow:"hidden"});
-        gsap.set(".section-2", {display:"none"});
-    }
-    }
+        gsap.fromTo(".section-2", {opacity:0} , {opacity:1,x:"-500",delay:0.10, stagger:-10})
+        
+     }
+  }
+});
+
+
+
+// reverse burger
+const revburger = document.querySelector(".go-back");
+revburger.addEventListener('click' , ()=>{
+    revburger.classList.toggle('active');
+    if(revburger.classList.contains("active")){
+        gsap.to(".nav-list", {x:"100%"});
+        gsap.to(".nav-list", {stroke:"white"})
+        gsap.set("body", {overflow:"auto"});
+        gsap.set("body", {overflowX:"hidden"});
+        gsap.set(".burger", {color:"black"});
+        gsap.to(".section-2",{x:0});  
+       }
 
 });
+
+
 
 
 // animation onloading the window
